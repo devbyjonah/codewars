99e2737d4ae3383efa94004ed3991e66e0cb67a1
devbyjonah <jonahwagner231@gmail.com>
2022-03-13 12:34:45 -0700

    added documentation on slice method

--- javascript/kyu-7/hideNumber.js
+++ javascript/kyu-7/hideNumber.js
@@ -1,3 +1,6 @@
+// maskify accepts an inputted number (phone,credit,passkey, etc.)
+// maps all digits to '#' excluding the last four indexes
+
 function maskify(cc) {
-  return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4)
+  return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4) // uses negative index in slice() method to count from end
 }
