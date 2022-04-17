5be1c60c945c592c211d1c45e544e00440ae4030
devbyjonah <jonahwagner231@gmail.com>
2022-04-17 09:55:00 -0700

    added duplicateEncode kata

---
+++ javascript/kyu-6/duplicateEncoder.js
@@ -0,0 +1,14 @@
+function duplicateEncode(word){
+
+/*
+    maps each to '(' if > 1 occurences or ')' if === 1 occurence
+    checks if occurs > once by comparing the first and last index of the character
+    if firstIndex === lastIndex the character only occurs once
+*/
+
+    return word
+        .toLowerCase()
+        .split('')
+        .map((a, i, w) => w.indexOf(a) === w.lastIndexOf(a) ? '(':')')
+        .join('')
+}
