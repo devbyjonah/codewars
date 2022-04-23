2f79dd08434dd883678f32606c9e3d242ebdbafe
Jonah Wagner <jonahwagner231@gmail.com>
2022-04-22 23:58:34 -0700

    Update duplicateEncoder.js

--- javascript/kyu-6/duplicateEncoder.js
+++ javascript/kyu-6/duplicateEncoder.js
@@ -1,7 +1,7 @@
 function duplicateEncode(word){
 
 /*
-    maps each to '(' if > 1 occurences or ')' if === 1 occurence
+    maps each character to '(' if > 1 occurences or ')' if === 1 occurence
     checks if occurs > once by comparing the first and last index of the character
     if firstIndex === lastIndex the character only occurs once
 */
