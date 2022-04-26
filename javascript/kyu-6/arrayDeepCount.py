a09b58c64a0d112a1b3f26682e13cee60a814940
devbyjonah <jonahwagner231@gmail.com>
2022-04-26 13:33:00 -0700

    revised documentation for deepCount solution

--- javascript/kyu-6/arrayDeepCount.py
+++ javascript/kyu-6/arrayDeepCount.py
@@ -2,9 +2,9 @@ def deep_count(a):
 
     # Deep count uses recursion to count the layers of nested arrays
     # iterates through inital array checking for any nested arrays
-    # if a nested array is found the function calls itself with the argument being the nested array
+    # if a nested array is found the function calls itself with the  inner array as the argument
     # this continues until the function iterates all the way through its input array without finding a nested array
-    # the value that is returned that continues returning to the previous caller until the original call is returned
+    # this last call will then return its value to the previous caller until reaching the original function call
 
     count = 0
     for i in a:
