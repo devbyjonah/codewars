f942ed1a257618766d766bb0340b147852830bc3
devbyjonah <jonahwagner231@gmail.com>
2022-04-27 23:11:11 -0700

    fixed file locations for misplaced kata solutions

--- javascript/kyu-6/vowelCode.py
+++
@@ -1,9 +0,0 @@
-# encode maps all vowels to numbers in these pairs {a:1, e:2, i:3, o:4, u:5}
-# translate method accepts a hash map 
-# maketrans method generates a hash map using indexes of two strings equal in length
-
-def encode(st):
-    return st.translate(str.maketrans('aeiou', '12345'))
-    
-def decode(st):
-    return st.translate(str.maketrans('12345', 'aeiou'))
