2664a4e37c6d5a2b1db1e07ae99997877777c13f
devbyjonah <jonahwagner231@gmail.com>
2022-05-02 00:32:26 -0700

    added twoSum documentation

--- javascript/kyu-5/twoSum.js
+++ javascript/kyu-5/twoSum.js
@@ -1,8 +1,11 @@
-function twoSum(ints, s){
-  var seen = {}
+// twoSum is a popular coding problem for coding interviews
+// accepts array of integers and returns the leftmost pair of integers that sum to target integer
 
-  for (var i = 0; i < ints.length; ++i) {
-    if (seen[s - ints[i]]) return [s - ints[i], ints[i]];
-    seen[ints[i]] = true
+function twoSum(ints, s){
+  var seen = {} // seen object stores each integer as a property with value of true once seen
+
+  for (var i = 0; i < ints.length; ++i) { // iterate through input array
+    if (seen[s - ints[i]]) return [s - ints[i], ints[i]] // check if matching value is in seen object
+    seen[ints[i]] = true // add current value to seen object
   }
 }
