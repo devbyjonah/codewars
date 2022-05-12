c7da82454ace21c631f7ff22421f6d17b3ddf104
devbyjonah <jonahwagner231@gmail.com>
2022-05-12 15:15:35 -0700

    added documentation to titleCase kata solution

--- javascript/kyu-6/titleCase.js
+++ javascript/kyu-6/titleCase.js
@@ -1,8 +1,14 @@
+// the titleCase function accepts an unformatted title and a list of words that shouldn't be capitalized
+// the functions returns the original title formatted properly using this minorWords list
+
 function titleCase(title, minorWords) {
+  // create words array with each item === a word from the inputted title
   let words = title.toLowerCase().split(' ')
+  // check for undefined minorWords array
   if (minorWords !== undefined){
+    // map minor words to a new array with all words lowercase
     let lowercase = minorWords.split(' ').map(val => val.toLowerCase())
-    
+    // formats words using map() method and returns joined list
     return words.map((val, ind) => {
       if (ind === 0 || !lowercase.includes(val)){
         return val.slice(0, 1).toUpperCase() + val.slice(1).toLowerCase()
@@ -11,6 +17,7 @@ function titleCase(title, minorWords) {
       }
     }).join(' ')
   }else {
+    // returns title with every word capitalized in case of undefined minorWords array
     return title.split(' ').map(val => val.slice(0,1).toUpperCase() + val.slice(1).toLowerCase()).join(' ')
   }
 }
