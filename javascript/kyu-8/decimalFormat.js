fc1862a32602b954e76674ce313814b793b8c056
devbyjonah <jonahwagner231@gmail.com>
2022-05-25 14:04:17 -0700

    added documentation to the decimalFormat kata solution

--- javascript/kyu-8/decimalFormat.js
+++ javascript/kyu-8/decimalFormat.js
@@ -1,3 +1,6 @@
+
+// The function twoDecimalPlaces uses the toFixed method of numbers to round the number down to the decimal place.
+// this formatting is commonly used when dealing with money and rounding transactions to the correct amount of digits
 function twoDecimalPlaces(n) {
   return Number(n.toFixed(2))
 }
