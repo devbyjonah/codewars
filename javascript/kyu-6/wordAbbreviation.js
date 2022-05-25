e13e513209e98633458d6625858e50d9260ff7db
devbyjonah <jonahwagner231@gmail.com>
2022-05-25 14:52:29 -0700

    added documentation for wordAbbreviation kata solution

--- javascript/kyu-6/wordAbbreviation.js
+++ javascript/kyu-6/wordAbbreviation.js
@@ -1,7 +1,8 @@
-// repr accepts a single word and returns a string representation using the given abbreviation
+// repr accepts a single word and returns a string representation using the given abbreviation (first letter + (word.length - 2) + last letter)
 function repr(word){
+  // create string representation
   let string = word[0] + `${word.length - 2}` + word[word.length - 1]
-  if (string.slice(string.length - 1) === ','){
+  if (string.slice(string.length - 1) === ','){ // check for comma attached
     string = word[0] + `${word.length - 3}` + word[word.length - 2] + ','
   }
   return string
@@ -11,18 +12,18 @@ function repr(word){
 function abbreviate(string) {
   let words = string.split(' ')
   
-  words.forEach((val, ind) => {
+  words.forEach((val, ind) => { // iterate through words and split any strings with hyphen
     if (val.includes('-')){
       words[ind] = val.split('-')
     }
   })
-  
+  // return the words array mapping each value using the repr function
   return words.map(val => {
-    if (Array.isArray(val)){
+    if (Array.isArray(val)){ // check for hyphenated word split into array
       return repr(val[0]) + '-' + repr(val[1])
-    }else if(val.length > 3){
+    }else if(val.length > 3){ // check for length > 3
       return repr(val)
-    }else {
+    }else { // returns val as is if word.length <= 3
       return val
     }
   }).join(' ')
