51b7246f17cb3ba4fc6acef390b17208e7825e7e
jonah wagner <jonahwagner231@gmail.com>
2022-12-31 08:42:00 -0800

    added quick documentation

--- javascript/kyu-6/sha256.js
+++ javascript/kyu-6/sha256.js
@@ -1,5 +1,6 @@
+// crypto is a core node module, createHash hashes strings using various algos including sha-256
 const { createHash } = require('crypto');
-
+// returns a new hash using the sha256 hexadecimal algo
 function sha256 (string) {
 	return createHash('sha256').update(string).digest('hex');
 }
