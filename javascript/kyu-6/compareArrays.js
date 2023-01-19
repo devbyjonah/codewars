76c7bd769783ba888913e4056de51e8c8ff74346
jonah wagner <jonahwagner231@gmail.com>
2023-01-19 08:42:12 -0800

    added documentation for comparing arrays

--- javascript/kyu-6/compareArrays.js
+++ javascript/kyu-6/compareArrays.js
@@ -6,16 +6,19 @@
 
 function comp(array1, array2) {
   if (!array1 || !array2) return false;
+  // generate FrequencyMap object from second array(squared)
   let squaredMap = new FrequencyMap(array2);
-
+  // iterate over first array (non squared) checking if each item exists in squaredMap and has value > 0
   for (let i = 0; i < array1.length; i++) {
     let cur = array1[i];
     if (squaredMap[cur ** 2] && squaredMap[cur ** 2] > 0) {
       squaredMap[cur ** 2]--;
     } else {
+      // end loop and return false if number not in map or occurences === 0
       return false;
     }
   }
+  // check for leftover values in map
   const mapValues = Object.values(squaredMap);
   return mapValues.every((val) => val === 0);
 }
