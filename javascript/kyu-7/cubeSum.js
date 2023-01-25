0abca89a6b458b18d2bd7dd4a4d9f552629e60c3
jonah wagner <jonahwagner231@gmail.com>
2023-01-24 21:29:06 -0800

    added cube sum solution

---
+++ javascript/kyu-7/cubeSum.js
@@ -0,0 +1,36 @@
+// One liner using javascript array methods fill, map, and reduce
+function sumCubes(n) {
+  return new Array(n)
+    .fill(0)
+    .map((val, ind) => (ind + 1) ** 3)
+    .reduce((a, b) => a + b, 0);
+}
+// Using standard JS for loop
+function _sumCubes(n) {
+  let sum = 0;
+  for (let i = n; i > 0; i--) {
+    sum += i ** 3;
+  }
+  return sum;
+}
+// Using a while loop
+function _sumCubes(n) {
+  let sum = 0;
+  let current = n;
+  while (current > 0) {
+    sum += current ** 3;
+    current = current - 1;
+  }
+  return sum;
+}
+// Recursive solution
+function _sumCubes(n, sum = null) {
+  let cur = sum;
+  if (n === 0) return sum;
+  if (cur === null) {
+    cur = n ** 3;
+  } else {
+    cur += n ** 3;
+  }
+  return sumCubes(n - 1, cur);
+}
